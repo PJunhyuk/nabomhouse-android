a437554e7657fec4476814f39db6a1095dab8626
PJunhyuk <jgravity@naver.com>
2017-02-01 22:33:30 +0900

    Implement basic application using webview

---
+++ app/src/main/java/com/nabomhouse/nabomhouse/MainActivity.java
@@ -0,0 +1,46 @@
+package com.nabomhouse.nabomhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setLayout();
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://nabomhouse.com");
+        mWebView.setWebViewClient(new WebViewClientClass());
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    /*
+     * Layout
+     */
+    private void setLayout(){
+        mWebView = (WebView) findViewById(R.id.webview);
+    }
+}
